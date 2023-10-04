c0def82a5b7ffb34f55f57f0ad298794182bdd35
JuanPabloJR <jjimenez57@ucol.mx>
2023-10-04 10:23:16 -0600

    Primera version

---
+++ index.js
@@ -0,0 +1,23 @@
+//Declaracion de variables en Javascript moderno
+//let contandor = 0;
+//contador = 8;
+//const constante = 4;
+//constante = 10;
+//let
+//const
+
+//Alerta en Javascript
+//alert("Hola desde una alerta de javascript")
+//console.log("Esto es Javascript desde el HTML");
+//console.log(numero);
+//console.log(constante);
+
+//Concatenar
+let nombre = "Juan";
+let apellido = "Jimenez";
+console.log("Mi nombre es: "+nombre +" "+ apellido);
+
+//document.write("Hola en el documento desde js");
+
+let seleccion = document.querySelector('#seccion1');
+seleccion.innerHTML = "<h3>Mi nombre es</h3>";
